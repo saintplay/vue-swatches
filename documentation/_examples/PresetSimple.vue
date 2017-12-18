8cdeea761f2aa53ca6173f4c4cfb28d937dd6efe
Diego Jara <saintplay96@gmail.com>
2017-12-18 08:32:58 -0500

    add example PresetSimple

---
+++ documentation/_examples/PresetSimple.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <strong>Color:</strong>
+  <swatches v-model="color" colors="text-advanced" />
+</div>
+</template>
+
+<script>
+import Swatches from 'vue-swatches'
+
+export default {
+  components: {
+    Swatches
+  },
+  data () {
+    return {
+      color: null,
+    }
+  }
+}
+</script>
--- documentation/_examples/index.js
+++ documentation/_examples/index.js
@@ -1,7 +1,9 @@
 import Simple from './Simple'
+import PresetSimple from './PresetSimple'
 import InlineSimple from './InlineSimple'
 
 export {
   Simple,
+  PresetSimple,
   InlineSimple
 }
