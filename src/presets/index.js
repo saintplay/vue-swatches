a084ba57829422c067a5fa31f6ab6016a3e3846c
Diego Jara <saintplay96@gmail.com>
2017-12-23 22:39:38 -0500

    add material dark and material light presets

--- src/presets/index.js
+++ src/presets/index.js
@@ -2,10 +2,14 @@ import simple from './simple'
 import textSimple from './text-simple'
 import textAdvanced from './text-advanced'
 import materialSimple from './material-simple'
+import materialLight from './material-light'
+import materialDark from './material-dark'
 
 export default {
   'simple': simple,
   'text-simple': textSimple,
   'text-advanced': textAdvanced,
-  'material-simple': materialSimple
+  'material-simple': materialSimple,
+  'material-light': materialLight,
+  'material-dark': materialDark
 }
