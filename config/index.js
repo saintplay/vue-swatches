ee55b791db4506cdea1f13f0086359a4863794a7
Diego Jara <saintplay96@gmail.com>
2018-06-14 10:29:48 -0500

    fix docs build: assets public path

--- config/index.js
+++ config/index.js
@@ -37,7 +37,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-swatches',
     devtool: '#source-map',
     productionSourceMap: false
   }
