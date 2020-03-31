3b2c1febae9ef3b74423d2fcc36d5c41150fc74b
Diego Jara Palomino <saintplay96@gmail.com>
2020-03-31 12:35:45 -0500

    add v2 announcer

--- config/index.js
+++ config/index.js
@@ -37,7 +37,6 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-swatches',
     devtool: '#source-map',
     productionSourceMap: false
   }
